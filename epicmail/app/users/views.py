193d7a118643b7fb6f805fd7969622931e2a5391
Wabuluka <dwabuluka@gmail.com>
2019-03-15 22:01:38 +0300

    validate user [chore #164592359]

--- epicmail/app/users/views.py
+++ epicmail/app/users/views.py
@@ -5,23 +5,48 @@ from epicmail.app import bcrypt
 from flask.views import MethodView
 from epicmail.app.models import User, users
 
+# special validatiors
+from epicmail.app.handlers.validatiors import Validations
+
 
 user_blueprint = Blueprint('user', __name__)
 
 class RegisterUser(MethodView):
 
     """Method View for creating new accounts"""
-
+    
     def post(self):
         # get data from json
         data_posted = request.get_json()
-
+        # check for user existence
+        for user in users:
+            if user['email'] == data_posted['email']:
+                responseObject = {
+                    'status': 202,
+                    'message': 'User with that email already exists, please try with another email'
+                }
+                return make_response(jsonify(responseObject)), 202
+        
         if request.method == "POST":
             user_id=len(users)+1
             firstname = data_posted['firstname']
             lastname = data_posted['lastname']
             email = data_posted['email']
             password_hash = data_posted['password_hash']
+
+            # validating the necessary fields
+            val_firstname = User.validate_firstname(firstname)
+            val_lastname = User.validate_lastname(lastname)
+            val_password = User.validate_password(password_hash)
+            val_email = User.validate_email(email)
+            if val_firstname:
+                return val_firstname
+            elif val_lastname:
+                return val_lastname
+            elif val_password:
+                return val_password
+            elif val_email:
+                return val_email
 
             # create a new user instance
             new_user = User(
@@ -34,7 +59,7 @@ class RegisterUser(MethodView):
             users.append(new_user.to_dictionary())
             auth_token = new_user.encode_auth_token(new_user.user_id)
             responseObject = {
-                "status": '201',
+                "status": 201,
                 "message": "You have successfully created an account",
                 "token": auth_token.decode(),
                 "data": new_user.to_dictionary()
@@ -59,17 +84,24 @@ class LoginUser(MethodView):
 
 
         for user in users:
-            if user. email == email:
-                bcrypt.check_password_hash(user.password_hash, password_hash)
-                # logged_in = user
+            if user['email'] == email:
+                bcrypt.check_password_hash(user['password'], password_hash)
+                auth_token = user.encode_auth_token(user.user_id)
                 responseObject = {
-                    'status': 'success',
-                    'message': 'Successfully logged in.'
+                    'status': 200,
+                    'message': 'Successfully logged in.',
+                    'token': auth_token.decode()
                 }
                 return make_response(jsonify(responseObject)), 200
+            responseObject = {
+                'status': 401,
+                'message':'Unathorized, either email or password is incorrect'
+            }
+            return make_response(jsonify(responseObject)),401
+
         responseObject ={
-            'status': 'Not possible',
-            'message': 'there is something wrong.' 
+            'status': 404,
+            'message': 'Your account was not found.' 
         }
         return make_response(jsonify(responseObject)), 404
 
