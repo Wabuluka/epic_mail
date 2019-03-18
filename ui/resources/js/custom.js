95f35445666ee23a50f415d1e32ae9831d3a1e60
Wabuluka <dwabuluka@gmail.com>
2019-03-18 17:34:31 +0300

    ui re-organization

--- ui/resources/js/custom.js
+++
@@ -1,9 +0,0 @@
-// toggle between responsive class and top-navigation class
-function myFunction(){
-  var x = document.getElementById("myTopnav");
-  if (x.className === "top-navigation") {
-    x.className += " responsive";
-  } else {
-    x.className = "top-navigation";
-  }
-}
