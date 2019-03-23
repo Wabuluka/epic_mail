f45909e623e8631916ca76d10e21dd859643df50
Wabuluka <dwabuluka@gmail.com>
2019-03-23 15:42:51 +0300

    refactor [chore #164808166]

--- epicmail/app/handler/validators/message_validators.py
+++ epicmail/app/handler/validators/message_validators.py
@@ -4,31 +4,31 @@ from flask import jsonify
 def validate_subject(subject):
     if not subject:
         error = {
-            "status": 401,
+            "status": 400,
             "error":"You must fill the subject field."
         }
-        return jsonify(error), 401
+        return jsonify(error), 400
 
 
 def validate_message(message):
     if not message:
         error = {
-            "status": 401,
+            "status": 400,
             "error":"You must fill the message field."
         }
-        return jsonify(error), 401
+        return jsonify(error), 400
 
 def validate_address(address):
     if not address:
         error = {
-            "status": 401,
+            "status": 400,
             "error":"You must fill the address field."
         }
-        return jsonify(error), 401
+        return jsonify(error), 400
     address = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', address)
     if address  == None:
         error = {
-            'status':401,
+            'status':400,
             'error': 'Make sure your address is well written.'
         }
         return jsonify(error)
@@ -37,14 +37,14 @@ def validate_address(address):
 def validate_createdby(createdby):
     if not createdby:
         error = {
-            "status": 401,
+            "status": 400,
             "error":"You must fill the createdby field."
         }
-        return jsonify(error), 401
+        return jsonify(error), 400
     createdby = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', createdby)
     if createdby  == None:
         error = {
-            'status':401,
+            'status':400,
             'error': 'Make sure your createdby is well written.'
         }
         return jsonify(error)
--- epicmail/app/handler/validators/user_validators.py
+++ epicmail/app/handler/validators/user_validators.py
@@ -5,28 +5,28 @@ def validate_firstname(firstname):
     """Validation for the field of first name in the User model"""
     if not firstname:
         error = {
-            "status": 401,
+            "status": 400,
             "message":"You must provide the first name field"
         }
-        return jsonify(error), 401
+        return jsonify(error), 400
     elif len(str(firstname)) < 5:
         error = {
-            "status": 401,
+            "status": 400,
             "message":"Names accepted must be at least 5 characters"
         }
-        return jsonify(error), 401
+        return jsonify(error), 400
     elif not isinstance(firstname, str):
         error = {
-            "status": 401,
+            "status": 400,
             "message":"The name must be a string"
         }
-        return jsonify(error), 401
+        return jsonify(error), 400
     elif re.search("[0-9]", str(firstname)) or re.search("[$#@*&%!~`+=|':;.><,_-]", str(firstname)):
         error = {
-            "status": 401,
+            "status": 400,
             "message":"Do not use special characters and numbers on a name"
         }
-        return jsonify(error), 401
+        return jsonify(error), 400
 
 
 
@@ -34,52 +34,52 @@ def validate_lastname(lastname):
     """Validation for the field of last name in the User model"""
     if not lastname:
         error = {
-            "status": 401,
+            "status": 400,
             "message":"You must provide the last name field"
         }
-        return jsonify(error), 401
+        return jsonify(error), 400
     elif len(str(lastname)) < 5:
         error = {
-            "status": 401,
+            "status": 400,
             "message":"Names accepted must be at least 5 characters"
         }
-        return jsonify(error), 401
+        return jsonify(error), 400
     elif not isinstance(lastname, str):
         error = {
-            "status": 401,
+            "status": 400,
             "message":"The name must be a string"
         }
-        return jsonify(error), 401
+        return jsonify(error), 400
     elif re.search("[0-9]", str(lastname)) or re.search("[$#@*&%!~`+=|':;.><,_-]", str(lastname)):
         error = {
-            "status": 401,
+            "status": 400,
             "message":"Do not use special characters and numbers on a name"
         }
-        return jsonify(error), 401
+        return jsonify(error), 400
 
 
 def validate_password(password):
     if len(password) < 4:
         error = {
-            "status": 401,
+            "status": 400,
             "message":"Your password must have 5 characters and above"
         }
         return jsonify(error)
     elif re.search('[0-9]',password) is None:
         error = {
-            "status": 401,
+            "status": 400,
             "message":"Your password must at least a number in it"
         }
         return jsonify(error)
     elif re.search('[A-Z]',password) is None:
         error = {
-            "status": 401,
+            "status": 400,
             "message":"Your password must at least contain an uppercase character"
         }
         return jsonify(error)
     elif re.search("[$#@*&%!~`+=|':;.><,_-]",password) is None:
         error = {
-            "status": 401,
+            "status": 400,
             "message":"Your password must at least a special character"
         }
         return jsonify(error)
@@ -91,34 +91,15 @@ def validate_email(email):
         """
         if not email or email.isspace():
             return jsonify({
-                "status": 401,
+                "status": 400,
                 "message": "Fill in the email"
             })
         valid_email = re.compile(
             r"(^[a-zA-Z0-9_.]+@[a-zA-Z0-9]+\.[a-z]+$)")
         if not valid_email.match(email):
             return jsonify({
-                "status": 401,
+                "status": 400,
                 "message": "please input valid email"
             })
-    # email = re.match(r"[a-zA-Z0-9_.]+@[a-zA-Z0-9]+\.[a-z]+$)", email)
-    # if email 
-    # if email:
-    #     error = {
-    #         'status':401,
-    #         'message': 'Make sure your email is well written'
-    #     }
-    #     return jsonify(error)
-    # elif email == '':
-    #     error = {
-    #         'status':401,
-    #         'message': 'You must fill in the email field'
-    #     }
-    #     return jsonify(error)
-    # elif not email or email.isspace():
-    #     return jsonify({
-    #         "status": 401,
-    #         "message": "please input email"
-    #     })
-    # else:
+    
         return None
