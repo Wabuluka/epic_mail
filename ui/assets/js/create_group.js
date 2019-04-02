4cb5a843d3abf14e49b42a8c2ce401983614f2d7
Wabuluka <dwabuluka@gmail.com>
2019-04-02 11:19:50 +0300

    user create group [ft #165030795]

---
+++ ui/assets/js/create_group.js
@@ -0,0 +1,34 @@
+let create_group=document.getElementById('submit');
+
+let token=localStorage.getItem('token');
+let current_user=localStorage.getItem('email');
+create_group.addEventListener('click', CreateGroup);
+
+function CreateGroup(e){
+    e.preventDefault();
+    let group_name=document.getElementById('group_name').value;
+    let role=document.getElementById('role').value;
+    let createdby=current_user;
+
+let data={
+    group_name:group_name,
+    role:role,
+    createdby:createdby
+};
+fetch("http://127.0.0.1:5000/api/v2/groups",{
+        method:'POST',
+        headers:{
+            'Application':'application/json, text/plain,*/*',
+            'Content_type':'application/json',
+            Authorization:`Bearer ${token}`
+        },
+        body:JSON.stringify(data)
+    })
+    .then((response) => response.json())
+    .then(function(data){
+        if(data['message']==='You have successfully created a new group'){
+            window.location.replace('/create.html');
+        }
+    });
+
+}
